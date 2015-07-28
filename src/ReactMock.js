4a1b9f7f4872a7454ac67115f1eab3c4c2c59c2d
Sebastian Otto <sotto@wemakeweb.de>
2015-07-29 01:24:47 +0200

    modified createElement to return Node oder View

--- src/ReactMock.js
+++ src/ReactMock.js
@@ -1,7 +1,12 @@
 import { MomentumNode } from 'src/MomentumNode';
+import { isFunction } from 'src/utils';
 
 export default class ReactMock {}
 
-ReactMock.createElement = function(tagName, attrs, ...childs){
-	return new MomentumNode(tagName, attrs, ...childs);
+ReactMock.createElement = function(tagNameOrComponent, attrs, ...childs){
+	if(isFunction(tagNameOrComponent)){
+		return new tagNameOrComponent(attrs, ...childs);
+	} else {
+		return new MomentumNode(tagNameOrComponent, attrs, ...childs);
+	}
 };
