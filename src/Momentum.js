ad9a38d1400d74a8b5251b625566659ed529bef1
Sebastian Otto <sotto@wemakeweb.de>
2015-07-29 02:34:27 +0200

    added bundeled Momentum Namespace

---
+++ src/Momentum.js
@@ -0,0 +1,11 @@
+import MomentumApp from './MomentumApp';
+import MomentumView from './MomentumView';
+import MomentumRouter from './MomentumRouter';
+
+let Momentum = { 
+	App: MomentumApp,
+	View: MomentumView,
+	Router: MomentumRouter
+};
+
+export default Momentum;
