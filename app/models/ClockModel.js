4e14ffc49e3e5fe6001ea0f46170e706c5e670f3
Sebastian Otto <sotto@wemakeweb.de>
2015-08-13 14:50:29 +0200

    remove app folder

--- app/models/ClockModel.js
+++
@@ -1,9 +0,0 @@
-export default class ClockModel extends Momentum.Stores.RethinkStore {
-	static schema = {
-		hour: Momentum.Type.number,
-		minutes: Momentum.Type.number,
-		seconds: Momentum.Type.number
-	}
-
-	static tableName = 'ClockModel'
-}
--- app/models/TestModel.js
+++
@@ -1,8 +0,0 @@
-export default class TestModel extends Momentum.Stores.RethinkStore {
-	static schema = {
-		titel: Momentum.Type.string,
-		content: Momentum.Type.string
-	}
-
-	static tableName = 'TestModel'
-}
